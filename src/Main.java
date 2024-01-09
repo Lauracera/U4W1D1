1b4d2ad08ca7bdb2e46f6bb73431726fd8bfc134
Laura Ceragioli <141077304+Lauracera@users.noreply.github.com>
2024-01-09 15:49:31 +0100

    Fist commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //ESERCIZIO 1
+        System.out.println("Moltiplicazione: " + moltiplicazione(2, 3));
+        System.out.println("Concatenzaione: " + concatenazione("Ciao, sono Laura"));
+
+        String[] array = aggiungi( new String[])
+        }
+    }
+}
