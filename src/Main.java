25ea8a1f04c37764e28c6b5ab61bbd887d0f1cf9
Laura Ceragioli <141077304+Lauracera@users.noreply.github.com>
2024-01-09 15:52:53 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,16 @@ public class Main {
     public static void main(String[] args) {
 
         //ESERCIZIO 1
-        System.out.println("Moltiplicazione: " + moltiplicazione(2, 3));
-        System.out.println("Concatenzaione: " + concatenazione("Ciao, sono Laura"));
+        System.out.println("Moltiplicazione: " + Moltiplicazione(2, 3));
+        System.out.println("Concatenzaione: " + Concatenazione("Ciao, ", "sono Laura"));
 
-        String[] array = aggiungi( new String[])
+
         }
+
+        public static int Moltiplicazione(int x, int y){
+        return x*y;
+    }
+    public static String Concatenazione(String a, String b){
+        return a + " " + b;
     }
 }
